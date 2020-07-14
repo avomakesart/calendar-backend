d42601206e1def43644d1e5986c3dd6e5cf503a0
Alvaro Castillo <alvaro.castillo777@gmail.com>
2020-07-13 22:08:07 -0500

    Adding the v1 fullstack app

---
+++ public/precache-manifest.9e2289d872614f3325f41f5bf535351f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb17ad693aeb5a68d6e4751d24918d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "776fbee1c06daf97d9bf",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "84b6294f3f6f0bb1da3d",
+    "url": "/static/css/main.bf39a9dc.chunk.css"
+  },
+  {
+    "revision": "776fbee1c06daf97d9bf",
+    "url": "/static/js/2.bd163c9c.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd163c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b6294f3f6f0bb1da3d",
+    "url": "/static/js/main.5dcfe98a.chunk.js"
+  },
+  {
+    "revision": "3365454410a9ca82f300",
+    "url": "/static/js/runtime-main.29165fbf.js"
+  }
+]);
